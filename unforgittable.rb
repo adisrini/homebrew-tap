f401ee4fe7ff46458c58253832295d36284ce65f
Aditya Srinivasan <asrinivasan@cinchapi.com>
2016-07-16 01:31:05 +0530

    Added unforgittable formula

---
+++ unforgittable.rb
@@ -0,0 +1,12 @@
+class Unforgittable < Formula
+  desc ""
+  homepage "https://github.com/as577/unforgittable"
+  url "https://github.com/as577/unforgittable/archive/1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "898f347b06d17775935f4703e71a1d470564948d082db5d6cf241338723e6c44"
+
+  def install
+    bin.install "testscript"
+  end
+
+end
